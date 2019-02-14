6b68b94e732e8d75f00da91e346997607384a777
Kakashiutano <47592198+Kakashiutano@users.noreply.github.com>
2019-02-14 13:55:30 +0700

    `Files uploaded

---
+++ Lesson Practice - comparing non-vowels and vowels.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int b=0,c=0;
+	char input;
+	while(input!='!')
+	{
+		scanf(" %c",&input);
+		if(input == 'a' || input == 'e' || input =='i' || input == 'o' || input == 'u')
+			b +=1;
+		else if (input =='!')
+			break;
+		else
+			c +=1;
+	}
+	if(b>c)
+	printf("The vowels are more than non-vowels");
+	else if(b<c)
+	printf("The vowels are less than the non-vowels");
+	else
+	printf("The vowels and the non-vowels are equal");
+}
